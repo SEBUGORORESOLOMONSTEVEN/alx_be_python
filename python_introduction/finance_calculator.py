31d40b3b70f4d52d63eb57036c83fd9fbf34cac8
sebugororesolomonsteven <sebugororesalomonsteven@gmail.com>
2025-07-11 16:32:09 +0300

    finance calculator over a year with interest

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,13 @@
+monthly_income = int(input("Enter your monthly income: "))
+
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+
+annual_interest_rate = 0.05
+
+projected_annual_savings = (monthly_savings * 12) + (monthly_savings * 12 * annual_interest_rate)
+
+print("Your monthly savings are", monthly_savings)
+
+print("Projected savings after one year, with interest, is", int(projected_annual_savings))
